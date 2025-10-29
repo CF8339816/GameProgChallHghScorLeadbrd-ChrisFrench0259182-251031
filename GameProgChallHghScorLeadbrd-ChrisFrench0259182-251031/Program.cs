5bd7b2a2aaf4458054c3e551da906740ecdd9896
CF8339816 <chris_french_3dga@hotmail.com>
2025-10-29 14:20:26 -0300

    random , delay and entry prompt

--- GameProgChallHghScorLeadbrd-ChrisFrench0259182-251031/Program.cs
+++ GameProgChallHghScorLeadbrd-ChrisFrench0259182-251031/Program.cs
@@ -2,14 +2,64 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace GameProgChallHghScorLeadbrd_ChrisFrench0259182_251031
 {
     internal class Program
     {
+        static int Score;
+        static Random random = new Random();
+        static string[ , , ] initials;
+
+
+
+
+
         static void Main(string[] args)
         {
+
+            GenerateScore();
+            Thread.Sleep(1000);
+            Initials();
+
+
         }
+        //methods below
+        //m1
+        static void GenerateScore()
+        {
+            Score = random.Next(1, 1001);
+            Console.WriteLine($" score: {Score}");
+            Console.ReadKey(true);
+
+
+
+
+        }
+
+        //m2
+        static void Initials()
+        {
+            Console.ForegroundColor = ConsoleColor.DarkYellow;
+            Console.WriteLine("Please enter your 3 initials");
+            Console.ForegroundColor = ConsoleColor.Blue;
+            initials[0,1,2] = Console.ReadLine();
+           
+        }
+
+
+        //m3
+
+
+        //m4
+
+
+
+        //m5
+
+
+    
     }
 }
