86c225815d685aa7ecdd7578a4aa49450d607ced
CF8339816 <chris_french_3dga@hotmail.com>
2025-10-29 14:43:05 -0300

    debugging
    working on this some more later  sticking on  user input to the array  maybe use a list or something need to look into it some more.

--- GameProgChallHghScorLeadbrd-ChrisFrench0259182-251031/Program.cs
+++ GameProgChallHghScorLeadbrd-ChrisFrench0259182-251031/Program.cs
@@ -11,9 +11,13 @@ namespace GameProgChallHghScorLeadbrd_ChrisFrench0259182_251031
     {
         static int Score;
         static Random random = new Random();
-        static string[ , , ] initials;
+        static string[] initals = new string[3];
 
 
+
+        //static char initial1;
+        //static char initial2;
+        //static char initial3;
 
 
 
@@ -23,7 +27,8 @@ namespace GameProgChallHghScorLeadbrd_ChrisFrench0259182_251031
             GenerateScore();
             Thread.Sleep(1000);
             Initials();
-
+            Thread.Sleep(3000);
+            WriteScoreInitals();
 
         }
         //methods below
@@ -31,11 +36,13 @@ namespace GameProgChallHghScorLeadbrd_ChrisFrench0259182_251031
         static void GenerateScore()
         {
             Score = random.Next(1, 1001);
-            Console.WriteLine($" score: {Score}");
+            Console.ForegroundColor = ConsoleColor.DarkYellow;
+            Console.Write($" score:");
+            Console.WriteLine( Score);
             Console.ReadKey(true);
 
 
-
+            Console.ForegroundColor = ConsoleColor.White;
 
         }
 
@@ -43,15 +50,38 @@ namespace GameProgChallHghScorLeadbrd_ChrisFrench0259182_251031
         static void Initials()
         {
             Console.ForegroundColor = ConsoleColor.DarkYellow;
-            Console.WriteLine("Please enter your 3 initials");
+            Console.WriteLine("Please enter your First inital");
             Console.ForegroundColor = ConsoleColor.Blue;
-            initials[0,1,2] = Console.ReadLine();
-           
+            initals[0] = Console.ReadLine();
+            Console.ForegroundColor = ConsoleColor.DarkYellow;
+            Console.WriteLine("Please enter your Second inital");
+            Console.ForegroundColor = ConsoleColor.Blue;
+            initals[1] = Console.ReadLine();
+            Console.ForegroundColor = ConsoleColor.DarkYellow;
+            Console.WriteLine("Please enter your Third inital");
+            Console.ForegroundColor = ConsoleColor.Blue;
+            initals[3] = Console.ReadLine();
+            Console.ForegroundColor = ConsoleColor.White;
         }
 
 
         //m3
+        static void WriteScoreInitals()
+        {
+            Console.ForegroundColor = ConsoleColor.DarkYellow;
+            Console.WriteLine("High Scores ");
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.Write($"{Score} ");
+            Console.ForegroundColor = ConsoleColor.DarkYellow;
+            Console.Write($"- ");
+            Console.ForegroundColor = ConsoleColor.Blue;
+           foreach (string inital in initals)
+            {
+                Console.WriteLine(inital);
+            }
 
+            Console.ForegroundColor = ConsoleColor.White;
+        }
 
         //m4
 
