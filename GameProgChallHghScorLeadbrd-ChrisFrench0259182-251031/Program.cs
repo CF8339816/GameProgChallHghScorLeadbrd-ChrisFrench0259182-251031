970cc696739f59c8536fc98c0c857c74af02f9c2
CF8339816 <chris_french_3dga@hotmail.com>
2025-10-31 00:37:29 -0300

    had wtf working for values now it is not .

--- GameProgChallHghScorLeadbrd-ChrisFrench0259182-251031/Program.cs
+++ GameProgChallHghScorLeadbrd-ChrisFrench0259182-251031/Program.cs
@@ -13,8 +13,8 @@ namespace GameProgChallHghScorLeadbrd_ChrisFrench0259182_251031
         static int newScore;
         static Random random = new Random();
         // static string initals;
-       //static string plrInitls = ($"{initial1}.{initial2}.{initial3}.");
-        static string plrInitls;
+        //static string plrInitls;
+        static string plrInitls = ($"{initial1}.{initial2}.{initial3}.");
         static string scoreData = ($"{hScore}, {plrInitls}");
         static string initial1;
         static string initial2;
@@ -22,141 +22,60 @@ namespace GameProgChallHghScorLeadbrd_ChrisFrench0259182_251031
         //static char initial1;
         //static char initial2;
         //static char initial3;
-
+        //static char input;
+        static string input;
+        static List<string> ScoreInfo = new List<string>();
         static string filePath = "Scores.txt";
-        static string hScore;
+        static string hScore = newScore.ToString();
         static List<string> list;
         static void Main(string[] args)
         {
-            //string initals = Initials();
+
+           // do
+            //{
+
+                genScore();
+                Thread.Sleep(1000);
+                Initials();
+                Thread.Sleep(1000);
+                WriteScoreInitals();
+                Thread.Sleep(500);
+                scoreList();
+                Thread.Sleep(1000);
+                //wtFile();
+                //Thread.Sleep(1000);
+                rfFile();
+
+                Console.ForegroundColor = ConsoleColor.DarkYellow;
+                Console.WriteLine("Do you want to exit? Type y for yes then press enter, press any other key to continue.");
+
+                //char input = (char)Console.Read();
+                input = Console.ReadLine();
+
+                Console.ForegroundColor = ConsoleColor.Blue;
+                Console.WriteLine(input);
+
+                Console.ForegroundColor = ConsoleColor.DarkYellow;
+            //}
+            // while (char.ToLower(input) != 'y');
+          //  while (input.ToLower() != "y");
+
+         //   Console.ForegroundColor = ConsoleColor.White;
+          //  Console.WriteLine("Exiting program...");
 
 
 
-
-           // List<string> hScore = GenerateScore();
-            genScore();
-            Thread.Sleep(1000);
-           // List<string> initals = Initals();
-
-           Initials();
-            Thread.Sleep(3000);
-            WriteScoreInitals();
-            wtFile();
-            rfFile();
-
-           // SaveListToFile( filePath, List<string> hScore);
-           // WriteFileToConsole( filePath);
 
 
 
         }
         //methods below
-        ////m1
-        //static List<string> GenerateScore()
-        //{
-
-        //    List<string> hScore = new List<string>();
-
-
-        //    string Score = newScore.ToString();
-
-        //    do
-        //    {
-        //        newScore = random.Next(1, 1001);
-        //        if (!string.IsNullOrWhiteSpace(Score))
-        //        {
-
-        //            hScore.Add(Score);
-
-        //        }
-        //    } while (!string.IsNullOrWhiteSpace(Score));
-
-        //    return hScore;
-
-
-        //}
-
-
-        ////m4
-        ////static void Initals()
-        //static List<string> Initals()
-        //{
-        //        List<string> initals = new List<string>();
-        //        Console.WriteLine("Please enter 3 initals:");
-
-        //        string input;
-
-        //        do
-        //        {
-        //            input = Console.ReadLine();
-        //            if (!string.IsNullOrWhiteSpace(input))
-        //            {
-
-        //                initals.Add(input);
-
-        //            }
-        //        } while (!string.IsNullOrWhiteSpace(input));
-
-        //        return initals;
-
-        //}
-
-
-        ////m5
-        //static void SaveListToFile(string filePath, /*List<string> hScore,*/ List<string> list)
-        //{
-        //    try
-        //    {
-        //        File.WriteAllLines(filePath, list);
-        //       // Console.WriteLine($"{list.Count} Sets of Scores and Initals saved to {filePath}");
-        //    }
-        //    catch (Exception ex)
-        //    {
-        //        Console.WriteLine($"Error writing to file...");
-        //    }
-        //}
-
-        ////m6
-        //static void WriteFileToConsole(string filePath)
-        //{
-        //    if (File.Exists(filePath))
-        //    {
-        //        try
-        //        {
-        //           string[] lines = File.ReadAllLines(filePath);
-
-        //            Console.ReadKey(true);
-        //            foreach (string line in lines)
-        //            {
-        //                Console.WriteLine(line);
-        //            }
-
-
-
-        //            //foreach (string line in lines)
-        //            //{
-        //            //    Console.WriteLine(line);
-        //            //}
-        //        }
-        //        catch (Exception ex)
-        //        {
-        //            Console.WriteLine($"Error reading from file");
-        //        }
-        //    }
-        //    else
-        //    {
-        //        Console.WriteLine($"File {filePath} not found.");
-        //    }
-        //}
 
         //m7
 
 
+
         //m8
-
-
-
-
 
         static void genScore()
         {
@@ -164,9 +83,9 @@ namespace GameProgChallHghScorLeadbrd_ChrisFrench0259182_251031
             Console.ForegroundColor = ConsoleColor.DarkYellow;
             Console.Write($" score:");
             Console.WriteLine(newScore);
-            Console.ReadKey(true);
+           // Console.ReadKey(true);
 
-             Console.ForegroundColor = ConsoleColor.White;
+            Console.ForegroundColor = ConsoleColor.White;
         }
 
 
@@ -178,21 +97,21 @@ namespace GameProgChallHghScorLeadbrd_ChrisFrench0259182_251031
             Console.ForegroundColor = ConsoleColor.Blue;
             initial1 = Console.ReadLine();
             //initial1 = (char)Console.Read();
-            Console.ReadKey(true);
+            //Console.ReadKey(true);
 
             Console.ForegroundColor = ConsoleColor.DarkYellow;
             Console.WriteLine("Please enter your Second inital and press enter");
             Console.ForegroundColor = ConsoleColor.Blue;
             initial2 = Console.ReadLine();
             //initial2 = (char)Console.Read();
-            Console.ReadKey(true);
+            // Console.ReadKey(true);
 
             Console.ForegroundColor = ConsoleColor.DarkYellow;
             Console.WriteLine("Please enter your Third inital and press enter");
             Console.ForegroundColor = ConsoleColor.Blue;
             initial3 = Console.ReadLine();
             //initial3 = (char)Console.Read();
-            Console.ReadKey(true);
+            // Console.ReadKey(true);
 
 
             plrInitls = ($"{initial1}.{initial2}.{initial3}.");
@@ -210,15 +129,6 @@ namespace GameProgChallHghScorLeadbrd_ChrisFrench0259182_251031
             Console.Write($"- ");
             Console.ForegroundColor = ConsoleColor.Blue;
             Console.WriteLine(plrInitls);
-            //Console.Write(initial2);
-            //Console.Write(initial2);
-            //Console.WriteLine(initial3);
-
-
-            //foreach (string inital in initals)
-            //{
-            //    Console.WriteLine(inital);
-            //}
 
             Console.ForegroundColor = ConsoleColor.White;
         }
@@ -227,9 +137,11 @@ namespace GameProgChallHghScorLeadbrd_ChrisFrench0259182_251031
 
         static void wtFile()
         {
+
             try
             {
                 File.WriteAllText(filePath, scoreData);
+         
                 Console.WriteLine("Data saved successfully.");
             }
             catch (Exception ex)
@@ -257,22 +169,44 @@ namespace GameProgChallHghScorLeadbrd_ChrisFrench0259182_251031
         //m10
         static void scoreList()
         {
-       
-        List<string> ScoreInfo = new List<string> { scoreData, };
+
+            List<string> ScoreInfo = new List<string>();
 
 
-            try
+            for (int i = 0; i < 1; i++)
             {
-                File.WriteAllLines(filePath, ScoreInfo);
-                Console.WriteLine("List saved successfully.");
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine($"An error occurred: {ex.Message}");
+
+                ScoreInfo.Add(scoreData);
+
+                Console.WriteLine($"Generated score: {scoreData}. Added to list.");
             }
 
+            Console.WriteLine("\n--- All scores have been added. ---\n");
+
+
+            foreach (string score in ScoreInfo)
+            {
+                // Console.WriteLine(score);
+
+                try
+                {
+                    File.WriteAllText(filePath, scoreData);
+        
+                    Console.WriteLine("Data saved successfully.");
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"An error occurred: {ex.Message}");
+                }
+            }
         }
-
-
     }
+
+
+    
 }
+
+        
+
+
+
